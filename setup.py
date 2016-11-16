6e4ac798ec0ff481b9c40e38419fe66b69f3187a
Ihor Kalnytskyi <igor@kalnitsky.org>
2016-11-16 11:50:50 +0200

    Use setuptools python_requires option
    `python_requires` is a string corresponding to a version specifier
    (as defined in PEP 440) for the Python version, used to specify the
    Requires-Python defined in PEP 345 [1].
    
    [1]: https://www.python.org/dev/peps/pep-0345/#version-specifiers

--- setup.py
+++ setup.py
@@ -25,6 +25,7 @@ setup(
     packages=find_packages(exclude=['docs', 'tests*']),
     include_package_data=True,
     zip_safe=False,
+    python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*',
     setup_requires=[
         'pytest-runner',
     ],
